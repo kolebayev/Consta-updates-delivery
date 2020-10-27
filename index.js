8682456186b7c173b69e3c533881a5df07465f65
Ilya Kolebayev <kolebayev@gmail.com>
2020-10-27 18:17:26 +0300

    Update start options

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express')
 require('dotenv').config()
 const bodyParser = require('body-parser')
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.port || 3000
 
 const figma = require('./modules/figma/figma')
 const github = require('./modules/github/github')
